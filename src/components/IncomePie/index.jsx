7ffebf5568fc897a95b37a960b9f80fb6c63b4df
Camacho <rafaelcamachofitti15@gmail.com>
2024-03-04 11:49:27 -0300

    fix/ nome do grafico de salarios

--- src/components/IncomePie/index.jsx
+++ src/components/IncomePie/index.jsx
@@ -3,10 +3,10 @@ import { PieChart, ResponsiveChartContainer } from '@mui/x-charts';
 
 const IncomePie = ({qtdLabel1, qtdLabel2, qtdLabel3, qtdLabel4, qtdLabel5}) => {
   const data = [
-    { id: 0, value: qtdLabel1, label: "Até 2,9 Salários" },
-    { id: 1, value: qtdLabel2, label: "2,9 a 7,1 Salários" },
-    { id: 2, value: qtdLabel3, label: "7,1 a 22 Salários" },
-    { id: 3, value: qtdLabel4, label: "22+ Salários" },
+    { id: 0, value: qtdLabel1, label: "Até 2,9 Mil" },
+    { id: 1, value: qtdLabel2, label: "2,9 a 7,1 Mil" },
+    { id: 2, value: qtdLabel3, label: "7,1 a 22 Mil" },
+    { id: 3, value: qtdLabel4, label: "22+ Mil" },
   ];
     return (
       <PieChart
@@ -18,7 +18,7 @@ const IncomePie = ({qtdLabel1, qtdLabel2, qtdLabel3, qtdLabel4, qtdLabel5}) => {
           },
         ]}
         height={200}
-        width={400}
+        width={350}
       />
  
     );
